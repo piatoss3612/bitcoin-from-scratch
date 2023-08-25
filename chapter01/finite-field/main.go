328dd7bf804d4541684f2ef63872a30de5c7405c
Lee Hyohwak <crewe1746@naver.com>
2023-08-25 23:21:52 +0900

    Add comments to chapter01 finite field

--- chapter01/finite-field/main.go
+++ chapter01/finite-field/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
+// 유한체의 원소를 나타내는 구조체
 type FieldElement struct {
 	num   int
 	prime int
 }
 
+// 유한체의 원소를 생성하는 함수
 func New(num, prime int) (*FieldElement, error) {
 	if num >= prime || num < 0 {
 		return nil, fmt.Errorf("num %d not in field range 0 to %d", num, prime-1)
@@ -17,14 +19,17 @@ func New(num, prime int) (*FieldElement, error) {
 	return &FieldElement{num, prime}, nil
 }
 
+// 유한체의 원소를 문자열로 표현하는 함수 (Stringer 인터페이스 구현)
 func (f FieldElement) String() string {
 	return fmt.Sprintf("FieldElement_%d(%d)", f.prime, f.num)
 }
 
+// 유한체의 원소가 같은 유한체에 속하고 같은 값을 가지는지 확인하는 함수
 func (f FieldElement) Equals(other FieldElement) bool {
 	return f.num == other.num && f.prime == other.prime
 }
 
+// 유한체의 원소를 더하는 함수 (더한 결과는 같은 유한체에 속함)
 func (f FieldElement) Add(other FieldElement) (*FieldElement, error) {
 	if f.prime != other.prime {
 		return nil, fmt.Errorf("cannot add two numbers in different Fields %d, %d", f.prime, other.prime)
@@ -34,18 +39,21 @@ func (f FieldElement) Add(other FieldElement) (*FieldElement, error) {
 	return New(num, f.prime)
 }
 
+// 유한체의 원소를 빼는 함수 (뺀 결과는 같은 유한체에 속함)
 func (f FieldElement) Sub(other FieldElement) (*FieldElement, error) {
 	if f.prime != other.prime {
 		return nil, fmt.Errorf("cannot add two numbers in different Fields %d, %d", f.prime, other.prime)
 	}
 
 	num := (f.num - other.num) % f.prime
+	// 음수일 경우 prime을 더해줌
 	if num < 0 {
 		num += f.prime
 	}
 	return New(num, f.prime)
 }
 
+// 유한체의 원소를 곱하는 함수 (곱한 결과는 같은 유한체에 속함)
 func (f FieldElement) Mul(other FieldElement) (*FieldElement, error) {
 	if f.prime != other.prime {
 		return nil, fmt.Errorf("cannot add two numbers in different Fields %d, %d", f.prime, other.prime)
@@ -55,7 +63,9 @@ func (f FieldElement) Mul(other FieldElement) (*FieldElement, error) {
 	return New(num, f.prime)
 }
 
+// 유한체의 원소를 거듭제곱하는 함수 (거듭제곱한 결과는 같은 유한체에 속함)
 func (f FieldElement) Pow(exp int) (*FieldElement, error) {
+	// 지수가 음수일 경우 양수로 변환
 	if exp < 0 {
 		exp = exp%(f.prime-1) + (f.prime - 1)
 	}
@@ -64,15 +74,18 @@ func (f FieldElement) Pow(exp int) (*FieldElement, error) {
 	return New(num, f.prime)
 }
 
+// 유한체의 원소를 나누는 함수 (나눈 결과는 같은 유한체에 속함)
 func (f FieldElement) Div(other FieldElement) (*FieldElement, error) {
 	if f.prime != other.prime {
 		return nil, fmt.Errorf("cannot add two numbers in different Fields %d, %d", f.prime, other.prime)
 	}
 
+	// 페르마의 소정리를 이용하여 나눗셈을 곱셈으로 변환
 	num := (f.num * pow(other.num, f.prime-2, f.prime)) % f.prime
 	return New(num, f.prime)
 }
 
+// 거듭제곱을 구하는 함수 (분할정복을 이용)
 func pow(num, exp, mod int) int {
 	if exp == 0 {
 		return 1
