c745789dfea1c09a0693f5c20842c58ee9bf2743
Lee Hyohwak <crewe1746@naver.com>
2023-09-05 21:14:09 +0900

    Change visibility of Tx, TxIn, TxOut fields

--- chapter05/tx/tx.go
+++ chapter05/tx/tx.go
@@ -9,20 +9,20 @@ import (
 )
 
 type Tx struct {
-	version  int      // 트랜잭션의 버전
-	inputs   []*TxIn  // 트랜잭션의 입력 목록
-	outputs  []*TxOut // 트랜잭션의 출력 목록
-	lockTime int      // 트랜잭션의 유효 시점
-	testnet  bool     // 테스트넷인지 여부
+	Version  int      // 트랜잭션의 버전
+	Inputs   []*TxIn  // 트랜잭션의 입력 목록
+	Outputs  []*TxOut // 트랜잭션의 출력 목록
+	Locktime int      // 트랜잭션의 유효 시점
+	Testnet  bool     // 테스트넷인지 여부
 }
 
-func NewTx(version int, inputs []*TxIn, outputs []*TxOut, lockTime int, testnet bool) *Tx {
+func NewTx(version int, inputs []*TxIn, outputs []*TxOut, locktime int, testnet bool) *Tx {
 	tx := &Tx{
-		version:  version,
-		inputs:   inputs,
-		outputs:  outputs,
-		lockTime: lockTime,
-		testnet:  testnet,
+		Version:  version,
+		Inputs:   inputs,
+		Outputs:  outputs,
+		Locktime: locktime,
+		Testnet:  testnet,
 	}
 
 	return tx
@@ -30,7 +30,7 @@ func NewTx(version int, inputs []*TxIn, outputs []*TxOut, lockTime int, testnet 
 
 func (t Tx) String() string {
 	return fmt.Sprintf("tx: %s\nversion: %d\ninputs: %s\noutputs: %s\nlocktime: %d",
-		t.ID(), t.version, t.inputs, t.outputs, t.lockTime)
+		t.ID(), t.Version, t.Inputs, t.Outputs, t.Locktime)
 }
 
 func (t Tx) ID() string {
@@ -42,18 +42,18 @@ func (t Tx) Hash() []byte {
 }
 
 func (t Tx) Serialize() []byte {
-	result := utils.IntToLittleEndian(t.version, 4)                    // 버전
+	result := utils.IntToLittleEndian(t.Version, 4)                    // 버전
 	result = append(result, t.serializeInputs()...)                    // 입력 목록
 	result = append(result, t.serializeOutputs()...)                   // 출력 목록
-	result = append(result, utils.IntToLittleEndian(t.lockTime, 4)...) // 유효 시점
+	result = append(result, utils.IntToLittleEndian(t.Locktime, 4)...) // 유효 시점
 	return result
 }
 
 func (t Tx) serializeInputs() []byte {
-	result := utils.EncodeVarint(len(t.inputs)) // 입력 개수
+	result := utils.EncodeVarint(len(t.Inputs)) // 입력 개수
 
 	// 입력 개수만큼 반복하면서 각 입력을 직렬화한 결과를 result에 추가
-	for _, input := range t.inputs {
+	for _, input := range t.Inputs {
 		result = append(result, input.Serialize()...)
 	}
 
@@ -61,10 +61,10 @@ func (t Tx) serializeInputs() []byte {
 }
 
 func (t Tx) serializeOutputs() []byte {
-	result := utils.EncodeVarint(len(t.outputs)) // 출력 개수
+	result := utils.EncodeVarint(len(t.Outputs)) // 출력 개수
 
 	// 출력 개수만큼 반복하면서 각 출력을 직렬화한 결과를 result에 추가
-	for _, output := range t.outputs {
+	for _, output := range t.Outputs {
 		result = append(result, output.Serialize()...)
 	}
 
@@ -85,8 +85,8 @@ func (t Tx) Fee(fetcher *TxFetcher) (int, error) {
 func (t Tx) totalInput(fetcher *TxFetcher) (int, error) {
 	total := 0
 
-	for _, input := range t.inputs {
-		value, err := input.Value(fetcher, t.testnet)
+	for _, input := range t.Inputs {
+		value, err := input.Value(fetcher, t.Testnet)
 		if err != nil {
 			return 0, err
 		}
@@ -100,8 +100,8 @@ func (t Tx) totalInput(fetcher *TxFetcher) (int, error) {
 func (t Tx) totalOutput() int {
 	total := 0
 
-	for _, output := range t.outputs {
-		total += output.amount
+	for _, output := range t.Outputs {
+		total += output.Amount
 	}
 
 	return total
@@ -109,23 +109,23 @@ func (t Tx) totalOutput() int {
 
 // Transaction 입력을 나타내는 구조체
 type TxIn struct {
-	prevIndex int    // 이전 트랜잭션의 출력 인덱스
-	prevTx    string // 이전 트랜잭션의 해시
-	scriptSig string // 해제 스크립트
-	seqNo     int    // 시퀀스 번호
+	PrevIndex int    // 이전 트랜잭션의 출력 인덱스
+	PrevTx    string // 이전 트랜잭션의 해시
+	ScriptSig string // 해제 스크립트
+	SeqNo     int    // 시퀀스 번호
 }
 
 // TxIn 생성자 함수
 func NewTxIn(prevIndex int, prevTx, scriptSig string, seqNos ...int) *TxIn {
 	tx := &TxIn{
-		prevTx:    prevTx,
-		prevIndex: prevIndex,
-		scriptSig: scriptSig,
-		seqNo:     0xffffffff,
+		PrevTx:    prevTx,
+		PrevIndex: prevIndex,
+		ScriptSig: scriptSig,
+		SeqNo:     0xffffffff,
 	}
 
 	if len(seqNos) > 0 {
-		tx.seqNo = seqNos[0]
+		tx.SeqNo = seqNos[0]
 	}
 
 	return tx
@@ -133,20 +133,20 @@ func NewTxIn(prevIndex int, prevTx, scriptSig string, seqNos ...int) *TxIn {
 
 // TxIn의 문자열 표현을 반환하는 함수 (fmt.Stringer 인터페이스 구현)
 func (t TxIn) String() string {
-	return fmt.Sprintf("%s:%d", t.prevTx, t.prevIndex)
+	return fmt.Sprintf("%s:%d", t.PrevTx, t.PrevIndex)
 }
 
 // TxIn을 직렬화한 결과를 반환하는 함수
 func (t TxIn) Serialize() []byte {
-	result := utils.ReverseBytes(utils.StringToBytes(t.prevTx))
-	result = append(result, utils.IntToLittleEndian(t.prevIndex, 4)...)
+	result := utils.ReverseBytes(utils.StringToBytes(t.PrevTx))
+	result = append(result, utils.IntToLittleEndian(t.PrevIndex, 4)...)
 	// TODO: scriptSig를 직렬화한 결과를 result에 추가
-	result = append(result, utils.IntToLittleEndian(t.seqNo, 4)...)
+	result = append(result, utils.IntToLittleEndian(t.SeqNo, 4)...)
 	return result
 }
 
 func (t TxIn) FetchTx(fetcher *TxFetcher, testnet bool) (*Tx, error) {
-	return fetcher.Fetch(t.prevTx, testnet, false)
+	return fetcher.Fetch(t.PrevTx, testnet, false)
 }
 
 func (t TxIn) Value(fetcher *TxFetcher, testnet bool) (int, error) {
@@ -155,7 +155,7 @@ func (t TxIn) Value(fetcher *TxFetcher, testnet bool) (int, error) {
 		return 0, err
 	}
 
-	return tx.outputs[t.prevIndex].amount, nil
+	return tx.Outputs[t.PrevIndex].Amount, nil
 }
 
 func (t TxIn) ScriptPubKey(fetcher *TxFetcher, testnet bool) (string, error) {
@@ -164,27 +164,27 @@ func (t TxIn) ScriptPubKey(fetcher *TxFetcher, testnet bool) (string, error) {
 		return "", err
 	}
 
-	return tx.outputs[t.prevIndex].scriptPubKey, nil
+	return tx.Outputs[t.PrevIndex].ScriptPubKey, nil
 }
 
 type TxOut struct {
-	amount       int    // 금액
-	scriptPubKey string // 잠금 스크립트
+	Amount       int    // 금액
+	ScriptPubKey string // 잠금 스크립트
 }
 
 func NewTxOut(amount int, scriptPubKey string) *TxOut {
 	return &TxOut{
-		amount:       amount,
-		scriptPubKey: scriptPubKey,
+		Amount:       amount,
+		ScriptPubKey: scriptPubKey,
 	}
 }
 
 func (t TxOut) String() string {
-	return fmt.Sprintf("%d:%s", t.amount, t.scriptPubKey)
+	return fmt.Sprintf("%d:%s", t.Amount, t.ScriptPubKey)
 }
 
 func (t TxOut) Serialize() []byte {
-	result := utils.IntToLittleEndian(t.amount, 8)
+	result := utils.IntToLittleEndian(t.Amount, 8)
 	// TODO: scriptPubKey를 직렬화한 결과를 result에 추가
 	return result
 }
@@ -248,7 +248,7 @@ func (tf *TxFetcher) Fetch(txID string, testnet, fresh bool) (*Tx, error) {
 			if err != nil {
 				return nil, err
 			}
-			tx.lockTime = utils.LittleEndianToInt(rawHex[len(rawHex)-4:])
+			tx.Locktime = utils.LittleEndianToInt(rawHex[len(rawHex)-4:])
 		} else {
 			fmt.Println("here")
 			tx, err = ParseTx(rawHex, testnet)
@@ -259,6 +259,6 @@ func (tf *TxFetcher) Fetch(txID string, testnet, fresh bool) (*Tx, error) {
 
 		tf.cache[txID] = tx
 	}
-	tf.cache[txID].testnet = testnet
+	tf.cache[txID].Testnet = testnet
 	return tf.cache[txID], nil
 }
