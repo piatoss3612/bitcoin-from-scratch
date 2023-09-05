a4d81a4292fa41ca66f30c6f12d7c3e291938e4c
Lee Hyohwak <crewe1746@naver.com>
2023-09-05 17:13:10 +0900

    Update TxIn and Add Fee method to Tx

--- chapter05/tx/tx.go
+++ chapter05/tx/tx.go
@@ -71,6 +71,42 @@ func (t Tx) serializeOutputs() []byte {
 	return result // 직렬화한 결과를 반환
 }
 
+func (t Tx) Fee(fetcher *TxFetcher) (int, error) {
+	totalIn, err := t.totalInput(fetcher)
+	if err != nil {
+		return 0, err
+	}
+
+	totalOut := t.totalOutput()
+
+	return totalIn - totalOut, nil
+}
+
+func (t Tx) totalInput(fetcher *TxFetcher) (int, error) {
+	total := 0
+
+	for _, input := range t.inputs {
+		value, err := input.Value(fetcher, t.testnet)
+		if err != nil {
+			return 0, err
+		}
+
+		total += value
+	}
+
+	return total, nil
+}
+
+func (t Tx) totalOutput() int {
+	total := 0
+
+	for _, output := range t.outputs {
+		total += output.amount
+	}
+
+	return total
+}
+
 // Transaction 입력을 나타내는 구조체
 type TxIn struct {
 	prevIndex int    // 이전 트랜잭션의 출력 인덱스
@@ -107,6 +143,28 @@ func (t TxIn) Serialize() []byte {
 	// TODO: scriptSig를 직렬화한 결과를 result에 추가
 	result = append(result, utils.IntToLittleEndian(t.seqNo, 4)...)
 	return result
+}
+
+func (t TxIn) FetchTx(fetcher *TxFetcher, testnet bool) (*Tx, error) {
+	return fetcher.Fetch(t.prevTx, testnet, false)
+}
+
+func (t TxIn) Value(fetcher *TxFetcher, testnet bool) (int, error) {
+	tx, err := t.FetchTx(fetcher, testnet)
+	if err != nil {
+		return 0, err
+	}
+
+	return tx.outputs[t.prevIndex].amount, nil
+}
+
+func (t TxIn) ScriptPubKey(fetcher *TxFetcher, testnet bool) (string, error) {
+	tx, err := t.FetchTx(fetcher, testnet)
+	if err != nil {
+		return "", err
+	}
+
+	return tx.outputs[t.prevIndex].scriptPubKey, nil
 }
 
 type TxOut struct {
