89819fe016975ee4d9e8750bf8600e329d8d73be
Lee Hyohwak <crewe1746@naver.com>
2023-09-13 15:24:25 +0900

    Setup chapter07

---
+++ chapter07/ecc/element.go
@@ -0,0 +1,203 @@
+package ecc
+
+import (
+	"fmt"
+	"math/big"
+)
+
+// 유한체의 원소를 나타내는 구조체
+type fieldElement struct {
+	num   *big.Int
+	prime *big.Int
+}
+
+// 유한체의 원소를 생성하는 함수
+func NewFieldElement(num, prime *big.Int) (FieldElement, error) {
+	// num과 prime이 nil인 경우 유한체의 원소를 생성할 수 없음
+	if num == nil || prime == nil {
+		return nil, fmt.Errorf("num or prime cannot be nil")
+	}
+
+	// 유한체의 원소가 0보다 작거나 위수보다 크거나 같은 경우 유한체의 원소를 생성할 수 없음
+	if !inRange(num, prime) {
+		return nil, fmt.Errorf("num %s not in field range 0 to %s", num, prime.Sub(prime, big.NewInt(1)))
+	}
+
+	return &fieldElement{num, prime}, nil
+}
+
+// 유한체의 원소값을 반환하는 함수
+func (f fieldElement) Num() *big.Int {
+	return f.num
+}
+
+// 유한체의 위수를 반환하는 함수
+func (f fieldElement) Prime() *big.Int {
+	return f.prime
+}
+
+// 유한체의 원소를 문자열로 표현하는 함수 (Stringer 인터페이스 구현)
+func (f fieldElement) String() string {
+	return fmt.Sprintf("FieldElement_%s(%s)", f.prime, f.num)
+}
+
+// 유한체의 원소가 같은 유한체에 속하고 같은 값을 가지는지 확인하는 함수
+func (f fieldElement) Equal(other FieldElement) bool {
+	return f.equal(other)
+}
+
+// 유한체의 원소가 다른 유한체에 속하거나 다른 값을 가지는지 확인하는 함수
+func (f fieldElement) NotEqual(other FieldElement) bool {
+	return !f.equal(other)
+}
+
+// 유한체의 원소가 같은 유한체에 속하고 같은 값을 가지는지 확인하는 내부 함수
+func (f fieldElement) equal(other FieldElement) bool {
+	return sameBN(f.num, other.Num()) && sameBN(f.prime, other.Prime())
+}
+
+// 유한체의 원소를 더하는 함수 (더한 결과는 같은 유한체에 속함)
+func (f fieldElement) Add(other FieldElement) (FieldElement, error) {
+	num, err := f.add(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewFieldElement(num, f.prime)
+}
+
+// 유한체의 원소를 더하는 내부 함수 (더한 결과는 같은 유한체에 속함)
+func (f fieldElement) add(other FieldElement) (*big.Int, error) {
+	if !sameBN(f.prime, other.Prime()) {
+		return nil, fmt.Errorf("cannot add two numbers in different Fields %d, %d", f.prime, other.Prime())
+	}
+
+	return addBN(f.num, other.Num(), f.prime), nil
+}
+
+// 유한체의 원소를 빼는 함수 (뺀 결과는 같은 유한체에 속함)
+func (f fieldElement) Sub(other FieldElement) (FieldElement, error) {
+	num, err := f.sub(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewFieldElement(num, f.prime)
+}
+
+// 유한체의 원소를 빼는 내부 함수 (뺀 결과는 같은 유한체에 속함)
+func (f fieldElement) sub(other FieldElement) (*big.Int, error) {
+	if !sameBN(f.prime, other.Prime()) {
+		return nil, fmt.Errorf("cannot subtract two numbers in different Fields %d, %d", f.prime, other.Prime())
+	}
+
+	return subBN(f.num, other.Num(), f.prime), nil
+}
+
+// 유한체의 원소를 곱하는 함수 (곱한 결과는 같은 유한체에 속함)
+func (f fieldElement) Mul(other FieldElement) (FieldElement, error) {
+	num, err := f.mul(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewFieldElement(num, f.prime)
+}
+
+// 유한체의 원소를 곱하는 내부 함수 (곱한 결과는 같은 유한체에 속함)
+func (f fieldElement) mul(other FieldElement) (*big.Int, error) {
+	if !sameBN(f.prime, other.Prime()) {
+		return nil, fmt.Errorf("cannot multiply two numbers in different Fields %d, %d", f.prime, other.Prime())
+	}
+
+	return mulBN(f.num, other.Num(), f.prime), nil
+}
+
+// 유한체의 원소를 거듭제곱하는 함수 (거듭제곱한 결과는 같은 유한체에 속함)
+func (f fieldElement) Pow(exp *big.Int) (FieldElement, error) {
+	return NewFieldElement(f.pow(exp), f.prime)
+}
+
+func (f fieldElement) pow(exp *big.Int) *big.Int {
+	// 페르마의 소정리를 이용하여 지수를 줄이고 거듭제곱을 계산
+	return powBN(f.num, big.NewInt(0).Mod(exp, big.NewInt(0).Sub(f.prime, big.NewInt(1))), f.prime)
+}
+
+// 유한체의 원소를 나누는 함수 (나눈 결과는 같은 유한체에 속함)
+func (f fieldElement) Div(other FieldElement) (FieldElement, error) {
+	num, err := f.div(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewFieldElement(num, f.prime)
+}
+
+// 유한체의 원소를 나누는 내부 함수 (나눈 결과는 같은 유한체에 속함)
+func (f fieldElement) div(other FieldElement) (*big.Int, error) {
+	if !sameBN(f.prime, other.Prime()) {
+		return nil, fmt.Errorf("cannot divide two numbers in different Fields %d, %d", f.prime, other.Prime())
+	}
+
+	// 페르마의 소정리를 이용하여 나눗셈을 곱셈으로 변환
+	// a / b = a * (b ** (p - 2)) mod p
+	return mulBN(f.num, invBN(other.Num(), f.prime), f.prime), nil
+}
+
+// secp256k1 유한체의 원소 구조체
+type s256FieldElement struct {
+	fieldElement
+}
+
+// secp256k1 유한체의 원소를 생성하는 함수
+func NewS256FieldElement(num *big.Int) (FieldElement, error) {
+	if num == nil {
+		return nil, fmt.Errorf("num cannot be nil")
+	}
+
+	// 유한체의 원소가 유한체의 크기보다 크거나 같은 경우 에러
+	if !inRange(num, P) {
+		return nil, fmt.Errorf("num %s not in field range 0 to %s", num, P.Sub(P, big.NewInt(1)))
+	}
+
+	f := fieldElement{num, P}
+
+	return &s256FieldElement{f}, nil
+}
+
+// secp256k1 유한체의 원소를 더하는 함수 (더한 결과는 같은 유한체에 속함)
+func (f s256FieldElement) Add(other FieldElement) (FieldElement, error) {
+	num, err := f.add(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewS256FieldElement(num)
+}
+
+// secp256k1 유한체의 원소를 빼는 함수 (뺀 결과는 같은 유한체에 속함)
+func (f s256FieldElement) Sub(other FieldElement) (FieldElement, error) {
+	num, err := f.sub(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewS256FieldElement(num)
+}
+
+// secp256k1 유한체의 원소를 곱하는 함수 (곱한 결과는 같은 유한체에 속함)
+func (f s256FieldElement) Mul(other FieldElement) (FieldElement, error) {
+	num, err := f.mul(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewS256FieldElement(num)
+}
+
+// secp256k1 유한체의 원소를 거듭제곱하는 함수 (거듭제곱한 결과는 같은 유한체에 속함)
+func (f s256FieldElement) Pow(exp *big.Int) (FieldElement, error) {
+	return NewS256FieldElement(f.pow(exp))
+}
+
+// secp256k1 유한체의 원소를 나누는 함수 (나눈 결과는 같은 유한체에 속함)
+func (f s256FieldElement) Div(other FieldElement) (FieldElement, error) {
+	num, err := f.div(other)
+	if err != nil {
+		return nil, err
+	}
+	return NewS256FieldElement(num)
+}
---
+++ chapter07/tx/tx.go
@@ -0,0 +1,292 @@
+package tx
+
+import (
+	"chapter07/script"
+	"chapter07/utils"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// 트랜잭션을 나타내는 구조체
+type Tx struct {
+	Version  int      // 트랜잭션의 버전
+	Inputs   []*TxIn  // 트랜잭션의 입력 목록
+	Outputs  []*TxOut // 트랜잭션의 출력 목록
+	Locktime int      // 트랜잭션의 유효 시점
+	Testnet  bool     // 테스트넷인지 여부
+}
+
+// Tx 생성자 함수
+func NewTx(version int, inputs []*TxIn, outputs []*TxOut, locktime int, testnet bool) *Tx {
+	tx := &Tx{
+		Version:  version,
+		Inputs:   inputs,
+		Outputs:  outputs,
+		Locktime: locktime,
+		Testnet:  testnet,
+	}
+
+	return tx
+}
+
+// 트랜잭션의 문자열 표현을 반환하는 함수 (fmt.Stringer 인터페이스 구현)
+func (t Tx) String() string {
+	return fmt.Sprintf("tx: %s\nversion: %d\ninputs: %s\noutputs: %s\nlocktime: %d",
+		t.ID(), t.Version, t.Inputs, t.Outputs, t.Locktime)
+}
+
+// 16진수 문자열로 표현된 트랜잭션 ID를 반환하는 함수
+func (t Tx) ID() string {
+	return hex.EncodeToString(t.Hash())
+}
+
+// 트랜잭션의 해시를 반환하는 함수
+func (t Tx) Hash() []byte {
+	return utils.ReverseBytes(utils.Hash256(t.Serialize()))
+}
+
+// 트랜잭션을 직렬화한 결과를 반환하는 함수
+func (t Tx) Serialize() []byte {
+	result := utils.IntToLittleEndian(t.Version, 4)                    // 버전
+	result = append(result, t.serializeInputs()...)                    // 입력 목록
+	result = append(result, t.serializeOutputs()...)                   // 출력 목록
+	result = append(result, utils.IntToLittleEndian(t.Locktime, 4)...) // 유효 시점
+	return result
+}
+
+// 트랜잭션 입력 목록을 직렬화한 결과를 반환하는 함수
+func (t Tx) serializeInputs() []byte {
+	result := utils.EncodeVarint(len(t.Inputs)) // 입력 개수
+
+	// 입력 개수만큼 반복하면서 각 입력을 직렬화한 결과를 result에 추가
+	for _, input := range t.Inputs {
+		result = append(result, input.Serialize()...)
+	}
+
+	return result // 직렬화한 결과를 반환
+}
+
+// 트랜잭션 출력 목록을 직렬화한 결과를 반환하는 함수
+func (t Tx) serializeOutputs() []byte {
+	result := utils.EncodeVarint(len(t.Outputs)) // 출력 개수
+
+	// 출력 개수만큼 반복하면서 각 출력을 직렬화한 결과를 result에 추가
+	for _, output := range t.Outputs {
+		result = append(result, output.Serialize()...)
+	}
+
+	return result // 직렬화한 결과를 반환
+}
+
+// 트랜잭션의 수수료를 반환하는 함수
+func (t Tx) Fee(fetcher *TxFetcher) (int, error) {
+	totalIn, err := t.totalInput(fetcher)
+	if err != nil {
+		return 0, err
+	}
+
+	totalOut := t.totalOutput()
+
+	return totalIn - totalOut, nil
+}
+
+// 트랜잭션 입력의 비트코인 총량을 반환하는 함수
+func (t Tx) totalInput(fetcher *TxFetcher) (int, error) {
+	total := 0
+
+	for _, input := range t.Inputs {
+		value, err := input.Value(fetcher, t.Testnet)
+		if err != nil {
+			return 0, err
+		}
+
+		total += value
+	}
+
+	return total, nil
+}
+
+// 트랜잭션 출력의 비트코인 총량을 반환하는 함수
+func (t Tx) totalOutput() int {
+	total := 0
+
+	for _, output := range t.Outputs {
+		total += output.Amount
+	}
+
+	return total
+}
+
+// 트랜잭션 입력을 나타내는 구조체
+type TxIn struct {
+	PrevIndex int            // 이전 트랜잭션의 출력 인덱스
+	PrevTx    string         // 이전 트랜잭션의 해시
+	ScriptSig *script.Script // 해제 스크립트
+	SeqNo     int            // 시퀀스 번호
+}
+
+// TxIn 생성자 함수
+func NewTxIn(prevTx string, prevIndex int, scriptSig *script.Script, seqNos ...int) *TxIn {
+	tx := &TxIn{
+		PrevTx:    prevTx,
+		PrevIndex: prevIndex,
+		ScriptSig: scriptSig,
+		SeqNo:     0xffffffff,
+	}
+
+	if len(seqNos) > 0 {
+		tx.SeqNo = seqNos[0]
+	}
+
+	return tx
+}
+
+// TxIn의 문자열 표현을 반환하는 함수 (fmt.Stringer 인터페이스 구현)
+func (t TxIn) String() string {
+	return fmt.Sprintf("%s:%d", t.PrevTx, t.PrevIndex)
+}
+
+// TxIn을 직렬화한 결과를 반환하는 함수
+func (t TxIn) Serialize() []byte {
+	result := utils.ReverseBytes(utils.StringToBytes(t.PrevTx))
+	result = append(result, utils.IntToLittleEndian(t.PrevIndex, 4)...)
+	// TODO: scriptSig를 직렬화한 결과를 result에 추가
+	result = append(result, utils.IntToLittleEndian(t.SeqNo, 4)...)
+	return result
+}
+
+// TxIn의 이전 트랜잭션을 가져오는 함수
+func (t TxIn) FetchTx(fetcher *TxFetcher, testnet bool) (*Tx, error) {
+	return fetcher.Fetch(t.PrevTx, testnet, false)
+}
+
+// TxIn의 이전 트랜잭션 출력의 금액을 반환하는 함수
+func (t TxIn) Value(fetcher *TxFetcher, testnet bool) (int, error) {
+	tx, err := t.FetchTx(fetcher, testnet)
+	if err != nil {
+		return 0, err
+	}
+
+	return tx.Outputs[t.PrevIndex].Amount, nil
+}
+
+// TxIn의 이전 트랜잭션 출력의 잠금 스크립트를 반환하는 함수
+func (t TxIn) ScriptPubKey(fetcher *TxFetcher, testnet bool) (*script.Script, error) {
+	tx, err := t.FetchTx(fetcher, testnet)
+	if err != nil {
+		return nil, err
+	}
+
+	return tx.Outputs[t.PrevIndex].ScriptPubKey, nil
+}
+
+// 트랜잭션 출력을 나타내는 구조체
+type TxOut struct {
+	Amount       int            // 금액
+	ScriptPubKey *script.Script // 잠금 스크립트
+}
+
+// TxOut 생성자 함수
+func NewTxOut(amount int, scriptPubKey *script.Script) *TxOut {
+	return &TxOut{
+		Amount:       amount,
+		ScriptPubKey: scriptPubKey,
+	}
+}
+
+// TxOut의 문자열 표현을 반환하는 함수 (fmt.Stringer 인터페이스 구현)
+func (t TxOut) String() string {
+	return fmt.Sprintf("%d:%s", t.Amount, t.ScriptPubKey)
+}
+
+// TxOut을 직렬화한 결과를 반환하는 함수
+func (t TxOut) Serialize() []byte {
+	result := utils.IntToLittleEndian(t.Amount, 8)
+	// TODO: scriptPubKey를 직렬화한 결과를 result에 추가
+	return result
+}
+
+// 트랜잭션을 가져오는 구조체
+type TxFetcher struct {
+	client *http.Client
+	cache  map[string]*Tx
+}
+
+// TxFetcher 생성자 함수
+func NewTxFetcher(clients ...*http.Client) *TxFetcher {
+	tf := &TxFetcher{
+		client: &http.Client{},
+		cache:  make(map[string]*Tx),
+	}
+
+	if len(clients) > 0 && clients[0] != nil {
+		tf.client = clients[0]
+	}
+
+	return tf
+}
+
+// 트랜잭션을 가져올 URL을 반환하는 함수
+func (tf TxFetcher) GetURL(testnet bool) string {
+	if testnet {
+		return "https://blockstream.info/testnet/api/"
+	}
+	return "https://blockstream.info/api/"
+}
+
+// 트랜잭션을 가져오는 함수
+func (tf *TxFetcher) Fetch(txID string, testnet, fresh bool) (*Tx, error) {
+	// fresh가 true이거나 tf.cache에 txID가 없으면 트랜잭션을 가져옴
+	if fresh || tf.cache[txID] == nil {
+		url := fmt.Sprintf("%s/tx/%s/hex", tf.GetURL(testnet), txID)
+
+		resp, err := tf.client.Get(url) // GET 요청을 보내 트랜잭션의 16진수 직렬화 결과를 가져옴
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("error fetching %s: %s", txID, resp.Status)
+		}
+
+		raw, err := io.ReadAll(resp.Body) // 응답 바디를 읽어서 raw에 저장
+		if err != nil {
+			return nil, err
+		}
+
+		rawHex := make([]byte, hex.DecodedLen(len(raw)))
+
+		_, err = hex.Decode(rawHex, raw) // raw를 16진수로 디코딩한 결과를 rawHex에 저장
+		if err != nil {
+			return nil, err
+		}
+
+		var tx *Tx
+
+		if rawHex[4] == 0x00 {
+			rawHex = append(rawHex[:4], rawHex[6:]...)
+			tx, err = ParseTx(rawHex, testnet)
+			if err != nil {
+				return nil, err
+			}
+			tx.Locktime = utils.LittleEndianToInt(rawHex[len(rawHex)-4:])
+		} else {
+			tx, err = ParseTx(rawHex, testnet)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		// 가져온 트랜잭션의 ID가 txID와 다르면 에러를 반환
+		if tx.ID() != txID {
+			return nil, fmt.Errorf("tx ID mismatch %s != %s", tx.ID(), txID)
+		}
+
+		tf.cache[txID] = tx // 가져온 트랜잭션을 tf.cache에 저장
+	}
+	tf.cache[txID].Testnet = testnet // 테스트넷 여부를 설정
+	return tf.cache[txID], nil       // 트랜잭션을 반환
+}
